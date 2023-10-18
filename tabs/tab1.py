edbac8b904b2910f26043e8bd5add241d21f36c4
curarin <145868208+curarin@users.noreply.github.com>
2023-10-18 15:33:21 +0200

    revert

--- tabs/tab1.py
+++ tabs/tab1.py
@@ -3,6 +3,8 @@ import functions.bq as bq
 import pandas as pd
 
 ### initialize session state stuff
+if 'my_dataframe' not in st.session_state:
+    st.session_state.my_dataframe = None
 if "choices" not in st.session_state:
     st.session_state.choices = []
 if "category" not in st.session_state:
@@ -15,15 +17,15 @@ if "category_sum" not in st.session_state:
     st.session_state.category_sum = None
 ######################################################
 
-def tab1(my_dataframe):
+def tab1():
     if st.session_state.my_dataframe_received == False:
         st.write("Nothing to see...")
     if st.session_state.my_dataframe_received == True:
 
         ### Filter Dataframe by Product Category
-        category = my_dataframe.groupby("category")["category"].apply(list).to_dict() #hier
-        choices = ("Kategorie", category) #hier
-        filtered_df = my_dataframe[my_dataframe['category'] == choices] #hier
+        st.session_state.category = st.session_state.my_dataframe.groupby("category")["category"].apply(list).to_dict()
+        st.session_state.choices = st.selectbox("Kategorie", st.session_state.category)
+        st.session_state.filtered_df = st.session_state.my_dataframe[st.session_state.my_dataframe['category'] == st.session_state.choices]
         columns_to_drop = ["barcode", "category", "date"]
         new_column_order = ["name", "quantity", "amount", "brand"]
         column_name_mapping = {"name": "Produkt Name",
@@ -31,22 +33,7 @@ def tab1(my_dataframe):
                                "amount": "Anzahl",
                                "brand": "Marke"
                               }   
-<<<<<<< HEAD
-        filtered_df = filtered_df.drop(columns=columns_to_drop) #hier
-        filtered_df = filtered_df[new_column_order].rename(columns=column_name_mapping) #hier
-        filtered_df = filtered_df.reset_index(drop=True) #hier
-        st.dataframe(filtered_df) #hier
-=======
         st.session_state.filtered_df = st.session_state.filtered_df.drop(columns=columns_to_drop)
         st.session_state.filtered_df = st.session_state.filtered_df[new_column_order].rename(columns=column_name_mapping)
         st.session_state.filtered_df = st.session_state.filtered_df.reset_index(drop=True)
         st.dataframe(st.session_state.filtered_df)
-
-        #st.session_state.category_sum = st.session_state.my_dataframe.groupby("category")["amount"].sum().reset_index()
-        #st.session_state.category_sum['amount'] = st.session_state.category_sum['amount'].astype(int)
-
-
-        # Now you can use 'category' as the x-axis values in st.bar_chart
-        #st.subheader("Übersicht aller Kategorien")
-        #st.bar_chart(st.session_state.category_sum, x="category", y="amount")
->>>>>>> parent of 0d05487 (Update tab1.py)
